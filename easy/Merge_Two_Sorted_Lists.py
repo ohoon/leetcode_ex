a4fbd7f0593809136fadc8cb81e87d42d4b6dead
ohoon <0hoon5661@gmail.com>
2020-10-15 20:36:21 +0900

    Clear

--- easy/Merge_Two_Sorted_Lists.py
+++ easy/Merge_Two_Sorted_Lists.py
@@ -55,11 +55,25 @@ Memory Usage: 14.2 MB, less than 99.99% of Python3 online submissions for Merge 
 '''
 
 '''
-여담
+다른 사람의 풀이
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if (not l1) or (l2 and (l1.val > l2.val)):
+            l1, l2 = l2, l1
+        
+        if l1:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        
+        return l1
 
 풀긴 풀었지만 시간은 1~2시간 걸렸다.
 링크드 리스트를 다루는 건 오랜만이라 머리가 많이 아팠음
 좀 더 이해하기 쉽게 코드를 짤 수 있었음 좋겠지만
 속도는 잘 나와서 만족한다.
+뒤늦게 어떤 사람의 코드를 봤는데
+와.. 너무 허무하다.
+재귀법을 쓰면 이렇게 코드가 간단해지구나 놀랍다.
+이 간단한 코드의 작동 과정을 이해하는 것도 만만치가 않다...ㅠㅠㅠ
 
 '''
