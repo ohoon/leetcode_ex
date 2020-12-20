6e3f4af88dae5c5cad31b54d5540e4a4a48925f2
ohoon <0hoon5661@gmail.com>
2020-12-20 19:12:04 +0900

    Clear

--- easy/Non-decreasing_Array.py
+++ easy/Non-decreasing_Array.py
@@ -9,13 +9,13 @@ class Solution:
         modify = False
         length = len(nums)
         for i in range(length - 1):
-            if nums[i] > nums[i+1]:         # increasing하면
+            if nums[i] > nums[i+1]:         # decreasing하면
                 if modify:                  # 이미 이전에 수정을 한번 했으면 False 반환
                     return False
                 
-                if i == 0 or nums[i-1] <= nums[i+1]:
+                if i == 0 or nums[i-1] <= nums[i+1]:        # i+1번째 요소값을 i번째에 대입하기 전에 i-1번째가 i+1번째 요소값보다 크면 increasing하지 않음
                     nums[i] = nums[i+1]
-                elif i == length - 2 or nums[i] <= nums[i+2]:
+                elif i == length - 2 or nums[i] <= nums[i+2]:   # 또는 i번째 요소값을 i+1번째에 대입하기 전에 i번째가 i+2번째 요소값보다 크면 increasing하지 않음
                     nums[i+1] = nums[i]
                 else:
                     return False
