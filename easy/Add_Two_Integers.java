e7e81d770008b8d999c924396ff7f3bd85aef0b5
Younghoon Kang <0hoon5661@gmail.com>
2022-04-11 19:09:49 +0900

    Clear

---
+++ easy/Add_Two_Integers.java
@@ -0,0 +1,16 @@
+/*
+2235.
+Add Two Integers
+https://leetcode.com/problems/add-two-integers/
+*/
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+/*
+Runtime: 1 ms, faster than 100.00% of Java online submissions for Add Two Integers.
+Memory Usage: 41.2 MB, less than 100.00% of Java online submissions for Add Two Integers.
+*/
