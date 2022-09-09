0d029672153efecaf0d8a9b2083736977c43aa98
Younghoon Kang <0hoon5661@gmail.com>
2022-09-09 19:44:10 +0900

    Clear

---
+++ medium/Subrectangle_Queries.java
@@ -0,0 +1,38 @@
+/*
+1476.
+Subrectangle Queries
+https://leetcode.com/problems/subrectangle-queries/
+*/
+
+class SubrectangleQueries {
+
+    private int[][] rect;
+    
+    public SubrectangleQueries(int[][] rectangle) {
+        rect = rectangle.clone();
+    }
+    
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                rect[i][j] = newValue;
+            }
+        }
+    }
+    
+    public int getValue(int row, int col) {
+        return rect[row][col];
+    }
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries obj = new SubrectangleQueries(rectangle);
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj.getValue(row,col);
+ */
+
+/*
+Runtime: 57 ms, faster than 9.17% of Java online submissions for Subrectangle Queries.
+Memory Usage: 56.2 MB, less than 15.53% of Java online submissions for Subrectangle Queries.
+*/
