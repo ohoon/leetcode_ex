cd6f97506a359b807fada24a7ccaafbabc4851f9
Younghoon Kang <0hoon5661@gmail.com>
2022-11-14 18:44:12 +0900

    Clear

---
+++ easy/Convert_the_Temperature.java
@@ -0,0 +1,11 @@
+/*
+2469.
+Convert the Temperature
+https://leetcode.com/problems/convert-the-temperature/
+*/
+
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.8 + 32};
+    }
+}
