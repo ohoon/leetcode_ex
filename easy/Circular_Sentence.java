b5341adad22b48e362a123dcfd5058415ddd806e
Younghoon Kang <0hoon5661@gmail.com>
2022-12-04 18:57:56 +0900

    Clear

---
+++ easy/Circular_Sentence.java
@@ -0,0 +1,19 @@
+/*
+2490.
+Circular Sentence
+https://leetcode.com/problems/circular-sentence/
+*/
+
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        String[] words = sentence.split(" ");
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            if (words[i].charAt(words[i].length()-1) != words[(i+1)%n].charAt(0)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
