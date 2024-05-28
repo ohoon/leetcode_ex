970612c19b45d310bd809ae6c06c7eba37608c75
ohoon <0hoon5661@gmail.com>
2024-05-28 16:07:22 +0900

    Clear

---
+++ easy/Find_the_Maximum_Achievable_Number.java
@@ -0,0 +1,11 @@
+/*
+2769.
+Find the Maximum Achievable Number
+https://leetcode.com/problems/find-the-maximum-achievable-number/
+*/
+
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2*t;
+    }
+}
